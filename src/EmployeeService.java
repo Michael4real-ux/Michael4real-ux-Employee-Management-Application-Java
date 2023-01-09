6a5b67ca41c51c55293ac8039bac762d4855c5bb
Decagon <decagon@Michaels-Babalola-MacBook-Air.local>
2023-01-09 02:07:13 +0100

    Employee class created and fields and method added

--- src/EmployeeService.java
+++ src/EmployeeService.java
@@ -1,2 +1,10 @@
+import java.util.HashSet;
+
 public class EmployeeService {
+    HashSet<Employee> employeeService = new HashSet<Employee>();
+
+    Employee emp1=new Employee(101, "Damilola", 24, "Developer", "IT", 25000);
+    Employee emp2=new Employee(102, "Meena", 26, "Tester","CO", 57000);
+    Employee emp3=new Employee(103, "Bob", 20, "DevOps Eng","Admin", 5000);
+    Employee emp4=new Employee(104, "Max", 27, "System Eng","CO",  70000);
 }
