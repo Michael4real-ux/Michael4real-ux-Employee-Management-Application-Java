8aa64d83c96b019dcf5a1132cf7e374a8d0f227d
Decagon <decagon@Michaels-Babalola-MacBook-Air.local>
2023-01-09 01:55:16 +0100

    App menu class added/configured and called at the entry point(main)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
+    EmployeeService service = new EmployeeService();
+
+    static boolean ordering = true;
+    public static void menu(){
+        System.out.println("**************Welcome To Employee Management System***************"
+                + "\n1. Add Employee"
+                + "\n2.View Employee"
+                + "\n3.Update Employee"
+                + "\n4. Delete Employee"
+                + "\n5.View All Employee"
+                + "\n6. Exist ");
+    };
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+     do{
+         menu();
+         System.out.printf("Enter your Choice: ");
+         int choice = scanner .nextInt();
+      switch(choice){
+          case 1:
+              System.out.println("Add Employee");
+              break;
+          case 2:
+              System.out.println("View Employee");
+              break;
+          case 3:
+              System.out.println("Update Employee");
+              break;
+          case 4:
+              System.out.println("Delete Employee");
+              break;
+          case 5:
+              System.out.println("View All Employee");
+              break;
+          case 6:
+              System.out.println("Thank you for using our employee management app");
+              System.exit(0);
+          default:
+              System.out.println("Please enter valid choice");
+
+      }
+     } while(ordering);
     }
 }
